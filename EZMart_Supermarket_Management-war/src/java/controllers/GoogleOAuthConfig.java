ed6ac97edd34eefe58b73eb194fa0d0812e48086
Trần BảoToàn <thomastran.fullstack@gmail.com>
2026-01-27 14:11:23 +0700

    commit

--- EZMart_Supermarket_Management-war/src/java/controllers/GoogleOAuthConfig.java
+++ EZMart_Supermarket_Management-war/src/java/controllers/GoogleOAuthConfig.java
@@ -18,7 +18,7 @@ public class GoogleOAuthConfig {
     
     public static final String CLIENT_ID = "46177555204-pse95ofks7srvnkuicvb2797i77dl9ab.apps.googleusercontent.com";
     public static final String CLIENT_SECRET = "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";  // Get from Google Cloud Console
-    public static final String REDIRECT_URI = "http://172.16.2.88:8080/EZMart_Supermarket_Management-war/oauth-callback";
+    public static final String REDIRECT_URI = "http://localhost:8080/EZMart_Supermarket_Management-war/oauth-callback";
     public static final String AUTHORIZATION_URL = "https://accounts.google.com/o/oauth2/v2/auth";
     public static final String TOKEN_URL = "https://oauth2.googleapis.com/token";
     public static final String USERINFO_URL = "https://www.googleapis.com/oauth2/v2/userinfo";
